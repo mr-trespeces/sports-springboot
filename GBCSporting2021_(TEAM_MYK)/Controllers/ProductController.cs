bbb6aaab223536150d85faaeb907a50b077d2b52
Kent Pedrocha <101266723@georgebrown.ca>
2021-02-24 12:16:14 -0500

    Product

---
+++ GBCSporting2021_(TEAM_MYK)/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using GBCSporting2021__TEAM_MYK_.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace GBCSporting2021__TEAM_MYK_.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private ProductContext context { get; set; }
+
+        public ProductController(ProductContext ctx)
+        {
+            context = ctx;
+        }
+        [HttpGet]
+        public IActionResult List()
+        {
+            var product = context.Products;
+            return View("List", product);
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var product = context.Products
+                .FirstOrDefault(c => c.ProductId == id);
+            return View(product);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Edit";
+            return View("Edit", new Product());
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var product = context.Products
+                .FirstOrDefault(c => c.ProductId == id);
+            return View(product);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products
+                .FirstOrDefault(c => c.ProductId == id);
+            return View("Delete", product);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            string action = (product.ProductId == 0) ? "Add" : "Edit";
+
+            if (ModelState.IsValid)
+            {
+                if (action == "Add")
+                {
+                    context.Products.Add(product);
+                }
+                else
+                {
+                    context.Products.Update(product);
+                }
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = action;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("List", "Product");
+        }
+    }
+}
--- GBCSporting2021_(TEAM_MYK)/Program.cs
+++ GBCSporting2021_(TEAM_MYK)/Program.cs
@@ -20,7 +20,7 @@ namespace GBCSporting2021__TEAM_MYK_
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("https://0.0.0.0:5001");
                 });
     }
 }
