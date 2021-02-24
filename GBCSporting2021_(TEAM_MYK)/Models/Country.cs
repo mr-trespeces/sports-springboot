505518ff5e903600390fe0ea91fcb498acca4ed6
Kent Pedrocha <101266723@georgebrown.ca>
2021-02-24 18:39:13 -0500

    Fixed Database

--- GBCSporting2021_(TEAM_MYK)/Models/Country.cs
+++ GBCSporting2021_(TEAM_MYK)/Models/Country.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,5 +11,12 @@ namespace GBCSporting2021__TEAM_MYK_.Models
         public int CountryId { get; set; }
 
         public string Name { get; set; }
+
+        public List<SelectListItem> Countries { get; } = new List<SelectListItem>
+    {
+        new SelectListItem { Value = "MX", Text = "Mexico" },
+        new SelectListItem { Value = "CA", Text = "Canada" },
+        new SelectListItem { Value = "US", Text = "USA"  },
+    };
     }
 }
