0b3fd22799c2699e649210bb5e3530341c6a9f28
Mark Trespeces <mr.trespeces@gmail.com>
2021-02-24 20:56:12 -0500

    - Start Up Incident Files -

--- GBCSporting2021_(TEAM_MYK)/Controllers/IncidentController.cs
+++ GBCSporting2021_(TEAM_MYK)/Controllers/IncidentController.cs
@@ -1,16 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GBCSporting2021__TEAM_MYK_.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace GBCSporting2021__TEAM_MYK_.Controllers
 {
     public class IncidentController : Controller
     {
-        public IActionResult Index()
+        private SportingContext context { get; set; }
+
+        public IncidentController(SportingContext ctx)
         {
-            return View();
+            context = ctx;
+        }
+        [HttpGet]
+        public IActionResult List()
+        {
+            var incident = context.Incidents;
+            return View("List", incident);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Incident());
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var incident = context.Incidents
+                .FirstOrDefault(c => c.IncidentId == id);
+            return View(incident);
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("List", "Incident");
+            }
+            Incident incident = context.Incidents.Find(id);
+            if (incident == null)
+            {
+                return RedirectToAction("List", "Incident");
+            }
+            return View("Delete");
+        }
+        [HttpPost]
+        public IActionResult Edit(Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                if (incident.IncidentId == 0)
+                {
+                    context.Incidents.Update(incident);
+                }
+                else
+                {
+                    context.Incidents.Update(incident);
+                }
+                context.SaveChanges();
+                return RedirectToAction("List", "Incident");
+            }
+            else
+            {
+                ViewBag.Action = (incident.IncidentId == 0) ? "Add" : "Edit";
+                return View(incident);
+            }
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult Delete(int id)
+        {
+            Incident incident = context.Incidents.Find(id);
+            context.Incidents.Remove(incident);
+            context.SaveChanges();
+            return RedirectToAction("List", "Incident");
         }
     }
+}
 }
