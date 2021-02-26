ad52877e3bcdc08a425c115b9b6b55805c8e2f3f
Kent Pedrocha <101266723@georgebrown.ca>
2021-02-26 14:22:25 -0500

    fixed

--- GBCSporting2021_(TEAM_MYK)/Models/Country.cs
+++ GBCSporting2021_(TEAM_MYK)/Models/Country.cs
@@ -9,7 +9,7 @@ namespace GBCSporting2021__TEAM_MYK_.Models
 {
     public class Country
     {
-        [Range(1, 10, ErrorMessage = "Please select a country")]
+        [Range(1, 10, ErrorMessage = "*Please select a country")]
         public int CountryId { get; set; }
         public string Name { get; set; }
     }
