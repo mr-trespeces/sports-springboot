dc44a96a879cf99d9eac5743dc8578578aca1ed8
Young Pyung Yoo <101254379@georgebrown.ca>
2021-02-26 13:46:37 -0500

    ..

--- GBCSporting2021_(TEAM_MYK)/Models/Technician.cs
+++ GBCSporting2021_(TEAM_MYK)/Models/Technician.cs
@@ -10,13 +10,13 @@ namespace GBCSporting2021__TEAM_MYK_.Models
     {
         public int TechnicianId { get; set; }
 
-        [Required(ErrorMessage = "Please enter a valid name")]
+        [Required(ErrorMessage = "*Please enter a valid name")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Please enter a valid email")]
+        [Required(ErrorMessage = "*Please enter a valid email")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Please enter a valid phone")]
+        [Required(ErrorMessage = "*Please enter a valid phone")]
         public string Phone { get; set; }
         public string Slug => Name?.Replace(' ', '-').ToLower();
     }
