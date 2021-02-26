024b631c23e4176eb8fe62c1b9eb2f5b8d45d1df
Mark Trespeces <mr.trespeces@gmail.com>
2021-02-26 13:30:14 -0500

    -- Product Done --

--- GBCSporting2021_(TEAM_MYK)/Models/Product.cs
+++ GBCSporting2021_(TEAM_MYK)/Models/Product.cs
@@ -10,11 +10,11 @@ namespace GBCSporting2021__TEAM_MYK_.Models
         public int ProductId { get; set; }
         [Required(ErrorMessage = "*Please enter a valid code.")]
         public string Code { get; set; }
-        [Required(ErrorMessage = "Please enter a valid name.")]
+        [Required(ErrorMessage = "*Please enter a valid name.")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Please enter a valid price.")]
+        [Required(ErrorMessage = "*Please enter a valid price.")]
         public string Price { get; set; }
-        [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString ="{0:MM/dd/yyyy}")]
+        [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString ="{0:yyyy/MM/dd}")]
         public DateTime? ReleaseDate { get; set; }
         public string Slug => Name?.Replace(' ', '-').ToLower();
     }
